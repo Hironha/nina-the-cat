77da0045c31cbf9039067eb629a94be351e21cc3
hironha <rafaelhirokato@gmail.com>
2022-12-27 08:02:01 -0300

    :sparkles: add track start event

---
+++ src/player-events/track-start.ts
@@ -0,0 +1,24 @@
+import { bold, Colors, EmbedBuilder, type TextBasedChannel } from 'discord.js';
+import { type PlayerEvents, type Queue, type Track } from 'discord-player';
+
+import { type PlayerEvent } from '@utils/player-event';
+
+class TrackStart implements PlayerEvent {
+	public once = false;
+	public name: keyof PlayerEvents = 'trackStart';
+
+	async execute(queue: Queue<any>, track: Track): Promise<void> {
+		if (!queue.metadata) return;
+		const channel = queue.metadata as TextBasedChannel;
+
+		const message = new EmbedBuilder()
+			.setColor(Colors.Blue)
+			.setTitle('😸 | Playing Now')
+			.setDescription(
+				`▶ | Started playing ${bold(track.title)} in ${bold(queue.connection.channel.name)}!`
+			);
+		channel.send({ embeds: [message] });
+	}
+}
+
+export default new TrackStart();
