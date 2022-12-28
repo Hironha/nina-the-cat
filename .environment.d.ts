2b0ea9d756df071f00fb78ab757253e1b7ce5f9d
hironha <rafaelhirokato@gmail.com>
2022-12-28 09:45:06 -0300

    :memo: update env variables

--- .environment.d.ts
+++ .environment.d.ts
@@ -1,6 +1,7 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
+			PUBLISH_COMMANDS?: 'true' | 'false'
 			DISCORD_BOT_TOKEN: string | undefined;
 			CLIENT_ID: string | undefined;
 			GUILD_ID: string | undefined;
