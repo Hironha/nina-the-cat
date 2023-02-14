30cf19a71c75070efc211367d13bd885fb5d1ae4
hironha <rafaelhirokato@gmail.com>
2023-02-14 20:23:02 -0300

    :recycle: add message handler pattern

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,5 @@
-import { EmbedBuilder, Colors, bold, italic } from 'discord.js';
-import { Command, type Commands, type ChatInputCommandInteraction } from '@utils/command';
+import { HelpHandler } from '@message-handlers/help-handler';
+import { Command, type ChatInputCommandInteraction } from '@utils/command';
 import { type DiscordClient } from '@utils/discord-client';
 
 class Help extends Command {
@@ -8,25 +8,12 @@ class Help extends Command {
 		this.setName('help').setDescription('List all available commands');
 	}
 
-	async execute(interaction: ChatInputCommandInteraction, client: DiscordClient) {
-		if (!interaction.isRepliable()) return;
+	async execute(interaction: ChatInputCommandInteraction, client: DiscordClient): Promise<void> {
+		if (interaction.isRepliable()) {
+			await interaction.deferReply();
+		}
 
-		const embedMessage = this.buildEmbedMessage(client.commands);
-		interaction.reply({ embeds: [embedMessage] });
-	}
-
-	private buildEmbedMessage(commands: Commands) {
-		const availableCommands = commands.map(command => ({
-			name: italic(bold(`/${command.name}`)),
-			value: command.description,
-			inline: false,
-		}));
-
-		return new EmbedBuilder()
-			.setColor(Colors.Blue)
-			.setTitle('😽 | Available Commands')
-			.setDescription('Here are the available commands, nya.')
-			.setFields(availableCommands);
+		new HelpHandler({ method: 'edit-reply' }).handle(interaction, client);
 	}
 }
 
---
+++ src/message-handlers/help-handler.ts
@@ -0,0 +1,47 @@
+import {
+	italic,
+	bold,
+	Colors,
+	EmbedBuilder,
+	type ChatInputCommandInteraction,
+	type CacheType,
+} from 'discord.js';
+
+import { type Commands } from '@utils/command';
+import { DiscordClient } from '@utils/discord-client';
+import { MessageHandler, type MessageHandlerOptions } from '@utils/message-handler';
+
+type Options = {};
+
+export class HelpHandler extends MessageHandler {
+	constructor(options?: MessageHandlerOptions<Options>) {
+		super(options?.method ?? 'reply');
+	}
+
+	async handle(
+		interaction: ChatInputCommandInteraction<CacheType>,
+		client: DiscordClient<boolean>
+	): Promise<void> {
+		if (interaction.isRepliable()) {
+			return super.reply(interaction, { embeds: this.buildEmbedMessage(client.commands) });
+		}
+
+		return super.handle(interaction, client);
+	}
+
+	private buildEmbedMessage(commands: Commands): EmbedBuilder[] {
+		const availableCommands = commands.map(command => ({
+			name: italic(bold(`/${command.name}`)),
+			value: command.description,
+			inline: false,
+		}));
+
+		const message = new EmbedBuilder()
+			.setColor(Colors.Blue)
+			.setTitle('😽 | Available Commands')
+			.setDescription('Here are the available commands, nya.')
+			.setFields(availableCommands);
+
+		return [message];
+	}
+}
