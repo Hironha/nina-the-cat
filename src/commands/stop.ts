41e3cf76aff53e7b2709acf5f28ad7593bbf8db7
hironha <rafaelhirokato@gmail.com>
2023-03-04 11:07:49 -0300

    :recycle: use message chain pattern

--- src/commands/stop.ts
+++ src/commands/stop.ts
@@ -1,8 +1,11 @@
-import { Colors, EmbedBuilder, type InteractionReplyOptions } from 'discord.js';
+import { IsPlayingHandler } from '@message-handlers/is-playing-handler';
+import { StopPlayerHandler } from '@message-handlers/stop-player-handler';
+import { InVoiceChannelHandler } from '@message-handlers/in-voice-channel-handler';
+import { SameVoiceChannelHandler } from '@message-handlers/same-voice-channel-handler';
+import { MessageHandlersChain } from '@utils/message-handler/message-handlers-chain';
 
-import { left, right, type Either } from '@utils/flow';
 import { type DiscordClient } from '@utils/discord-client';
-import { PlayerInteractionUtils } from '@utils/player-interaction';
+import { type MessageHandlerOptions } from '@utils/message-handler';
 import { Command, type ChatInputCommandInteraction } from '@utils/command';
 
 class Stop extends Command {
@@ -12,43 +15,18 @@ class Stop extends Command {
 	}
 
 	async execute(interaction: ChatInputCommandInteraction, client: DiscordClient): Promise<void> {
-		if (!interaction.isRepliable() || !client.player) return;
-
-		const validation = this.validateInteraction(interaction);
-		if (validation.isLeft()) return void interaction.reply(validation.value);
-
-		const guild = PlayerInteractionUtils.getGuild(interaction);
-		if (guild.isLeft()) return void interaction.reply(guild.value);
-
-		const queue = PlayerInteractionUtils.getPlayerQueue(client.player, guild.value.id);
-		if (queue.isLeft()) return void interaction.reply(queue.value);
-
-		if (!queue.value.playing) {
-			return void interaction.reply({ content: "I'm not playing any songs right now!" });
+		if (interaction.isRepliable()) {
+			await interaction.deferReply();
 		}
 
-		queue.value.clear();
-		queue.value.stop();
-		queue.value.destroy();
+		const options: MessageHandlerOptions<{}> = { method: 'edit-reply' };
 
-		interaction.reply({ embeds: [this.buildStopMessage()] });
-	}
-
-	private validateInteraction(
-		interaction: ChatInputCommandInteraction
-	): Either<InteractionReplyOptions, null> {
-		if (!PlayerInteractionUtils.isFromListener(interaction)) {
-			return left({ content: "You're not in the same voice channel as me, dummy" });
-		}
-
-		return right(null);
-	}
-
-	private buildStopMessage() {
-		return new EmbedBuilder()
-			.setColor(Colors.Blue)
-			.setTitle('🐱 | Stop')
-			.setDescription('I stopped playing the song and cleared the queue');
+		new MessageHandlersChain(new InVoiceChannelHandler(options))
+			.next(new SameVoiceChannelHandler(options))
+			.next(new IsPlayingHandler(options))
+			.next(new StopPlayerHandler(options))
+			.build()
+			.handle(interaction, client);
 	}
 }
 
